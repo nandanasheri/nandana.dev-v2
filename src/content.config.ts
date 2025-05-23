e606404454299aa652953e1ccf981d48f91a0740
Nandana <nandanasheri@gmail.com>
2025-05-23 16:28:41 -0500

    now page - done?

---
+++ src/content.config.ts
@@ -0,0 +1,26 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob } from 'astro/loaders';
+
+// 3. Define your collection(s)
+const posts = defineCollection({
+    loader: glob({ pattern: "**/*.md", base: "./src/content/posts" }),
+    schema: z.object({
+    title: z.string(),
+    time: z.string(),
+    date: z.string()
+  }),
+});
+
+const pages = defineCollection({
+    loader: glob({ pattern: "**/*.md", base: "./src/content/pages" }),
+    schema: z.object({
+    title: z.string(),
+    date: z.string()
+  }),
+});
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { posts, pages };
---
+++ src/content/pages/now.md
@@ -0,0 +1,9 @@
+---
+title: 'now'
+date: 'May 23, 2025'
+---
+- Graduated from UIC!
+- Doing Research in Biomedical NLP under Dr. Sweta Yadav
+- Redesigning my personal website
+- Preparing for the recruiting season with Leetcode
+- Reading [A Handmaid’s Tale](https://en.wikipedia.org/wiki/The_Handmaid's_Tale)!
